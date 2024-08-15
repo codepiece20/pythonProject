6596620c28f678835dcfa81c28970c18524702c0
codepiece20 <codepiece20@gmail.com>
2024-08-15 23:56:48 +0500

    Add files via upload

---
+++ pythonProject/BasicDataTypes.py
@@ -0,0 +1,16 @@
+# String Data Type
+name = "Zoro"
+
+# Integer Data Type
+age = 21
+
+# Float Data Type
+height = 180.3
+
+# Boolean Data Type
+active = True
+
+print(name)
+print(age)
+print(height)
+print(active)
